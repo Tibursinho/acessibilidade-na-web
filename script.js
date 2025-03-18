2ea4ac6a35f738dc1d95b66ecc2137516a6ec2ba
davi campos jaremczuk <147657664+Tibursinho@users.noreply.github.com>
2025-03-18 13:21:54 -0300

    dsa

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const alternaContraste = document.getElementById('alterna-contraste')
-
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
