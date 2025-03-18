34213421018cd782b5698bd3c6791c8a0fab4a98
davi campos jaremczuk <147657664+Tibursinho@users.noreply.github.com>
2025-03-18 13:25:17 -0300

    ,

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const alternaContraste = document.getElementById('alterna-contraste')
-    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-    
+
     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
